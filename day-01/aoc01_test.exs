2cb3e3d068e12aeb42eceaf5c55d98b8e1e80bca
Martin Hueser <hueser@gmx.net>
2018-12-02 16:16:08 +0100

    add solution day 1 second part do some refactoring

--- day-01/aoc01_test.exs
+++ day-01/aoc01_test.exs
@@ -1,21 +1,64 @@
 Code.load_file("aoc01.exs", __DIR__)
 
 ExUnit.start()
-ExUnit.configure(exclude: :pending, trace: true)
+ExUnit.configure(exclude: [pending: true], trace: true)
 
 defmodule SumTest do
   use ExUnit.Case
 
+  defp create_testinput(list, file) do
+    list
+    |> Enum.map(&Integer.to_string/1)
+    |> Enum.map(fn s -> s <> "\n" end)
+    |> Enum.into(File.stream!(file))
+  end
+
+  @tag pending: false
   test "sum ints in file" do
-    [1,1,1] |> Enum.map(&Integer.to_string/1) |> Enum.map(fn s -> s <> "\n" end) |> Enum.into(File.stream!("testinput.txt"))
-    assert Aoc01.sum_file("testinput.txt") == 3
+    [1, 1, 1] |> create_testinput("testinput.txt")
+
+    assert AoC01.get_frequency("testinput.txt") == 3
   end
+
+  @tag pending: false
   test "sum ints in file with negatives" do
-    [1,1,-2] |> Enum.map(&Integer.to_string/1) |> Enum.map(fn s -> s <> "\n" end) |> Enum.into(File.stream!("testinput.txt"))
-    assert Aoc01.sum_file("testinput.txt") == 0
+    [1, 1, -2] |> create_testinput("testinput.txt")
+
+    assert AoC01.get_frequency("testinput.txt") == 0
   end
+
+  @tag pending: false
   test "sum ints in file with only negatives" do
-    [-1,-2,-3] |> Enum.map(&Integer.to_string/1) |> Enum.map(fn s -> s <> "\n" end) |> Enum.into(File.stream!("testinput.txt"))
-    assert Aoc01.sum_file("testinput.txt") == -6
+    [-1, -2, -3] |> create_testinput("testinput.txt")
+
+    assert AoC01.get_frequency("testinput.txt") == -6
+  end
+
+  @tag pending: false
+  test "get repeated frequency one pass" do
+    [1, -1] |> create_testinput("testinput.txt")
+
+    assert AoC01.get_first_repeated_frequency("testinput.txt") == 0
+  end
+
+  @tag pending: false
+  test "get repeated frequency multiple passes 1" do
+    [3, 3, 4, -2, -4] |> create_testinput("testinput.txt")
+
+    assert AoC01.get_first_repeated_frequency("testinput.txt") == 10
+  end
+
+  @tag pending: false
+  test "get repeated frequency multiple passes 2" do
+    [-6, 3, 8, 5, -6] |> create_testinput("testinput.txt")
+
+    assert AoC01.get_first_repeated_frequency("testinput.txt") == 5
+  end
+
+  @tag pending: false
+  test "get repeated frequency multiple passes 3" do
+    [7, 7, -2, -7, -4] |> create_testinput("testinput.txt")
+
+    assert AoC01.get_first_repeated_frequency("testinput.txt") == 14
   end
 end
